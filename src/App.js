368b75d842e1e0eebda1a9302692aa1caaf9643b
Matheus Alves Lemos <matheus@osm.com.br>
2020-08-23 22:44:51 -0300

    Implementação dos métodos Adicionar e Deletar repositories

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from "react";
-
+import api from './services/api';
 import "./styles.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  
+ const [repositories,setRepositories] = useState([]);
+
+ useEffect(()=>{
+  api.get('repositories').then(response => {
+    setRepositories(response.data);
+  });
+ },[]);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories',{
+      url: "https://github.com/matheus1987",
+      title: `Gabi ${Date.now()}`,
+       techs: ["Direito Trabalhista", "OAB", "JSON"]
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories,repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
-  }
+    const response = await api.delete(`repositories/${id}`);
+    if (response.status === 204){
+        setRepositories(repositories.filter(
+          repository => repository.id !== id
+        ));
+        }
+    }
+  
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository=><li key={repository.id}>{repository.title}
+        
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+        </li>)}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
